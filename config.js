9c63696cf2f8ab1b25b52cae4d5e0d01a3a15f09
Mwonu <88635576+Mwonu@users.noreply.github.com>
2026-01-29 05:08:35 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,23 @@
 /**
  * LIZA-AI V2 Configuration
- * Developer: Chank!nd3 p4d4y41!
+ * Developer: chank!nd3 p4d4y41!
  */
 
 module.exports = {
     SESSION_ID: process.env.SESSION_ID || '', 
-    OWNER_NUMBER: '91...', // ഇവിടെ നിങ്ങളുടെ നമ്പർ 91 വെച്ച് തന്നെ തുടങ്ങണം (+ വേണ്ട)
-    OWNER_NAME: 'Chank!nd3 p4d4y41!',
+    OWNER_NUMBER: process.env.OWNER_NUMBER || '91...', // റെയിൽവേ വേരിയബിൾ വഴിയോ നേരിട്ടോ നൽകാം
+    OWNER_NAME: 'chank!nd3 p4d4y41!',
     BOT_NAME: 'LIZA-AI V2',
     
     // Prefix സെറ്റിംഗ്സ്
     PREFIX: process.env.PREFIX || '.', 
     
+    // റെയിൽവേയിൽ NO_PREFIX = true എന്ന് നൽകിയാൽ ചിഹ്നം ഇല്ലാതെ വർക്ക് ചെയ്യും
+    NO_PREFIX: process.env.NO_PREFIX === 'true' ? true : false, 
+    
     MODE: process.env.MODE || 'public',
     
     // സ്റ്റിക്കർ മെറ്റാഡേറ്റ
     PACKNAME: 'LIZA-AI V2',
-    AUTHOR: 'Chank!nd3 p4d4y41!'
+    AUTHOR: 'chank!nd3 p4d4y41!'
 };
