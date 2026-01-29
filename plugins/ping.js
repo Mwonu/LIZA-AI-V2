88d24a72467eb61a76677fce49d9ef9b965d5f13
Mwonu <88635576+Mwonu@users.noreply.github.com>
2026-01-29 04:36:23 +0400

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,7 +1,12 @@
+/**
+ * Ping Plugin - LIZA-AI V2
+ * Developer: chank!nd3 p4d4y41!
+ */
+
 module.exports = {
-    name: 'ping',
+    command: ['ping', 'p'], // ഇവിടെ 'command' എന്ന് തന്നെ വേണം
     category: 'main',
-    desc: 'ബോട്ടിന്റെ സ്പീഡ് അറിയാൻ',
+    description: 'ബോട്ടിന്റെ സ്പീഡ് അറിയാൻ',
     async execute(sock, m, { args }) {
         await sock.sendMessage(m.chat, { text: 'Pong! 🏓' }, { quoted: m });
     }
